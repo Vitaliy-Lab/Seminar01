94223cf6b89ce6ef261ed0369649ab98285d78d5
Vitaliy-lab <9175603000@mail.ru>
2023-09-23 13:32:34 +0300

    Добавил задачу1 в Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int number = new Random().Next(10, 100); // 99 + 1
+Console.WriteLine($"Случайное число из отрезка [10, 99] -> {number}");
+
+// int firstDigit = number / 10; // 78 / 10 = 7
+// int secondDigit = number % 10;
+
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшую цифру числа -> {firstDigit}");
+// else Console.WriteLine($"Наибольшую цифру числа -> {secondDigit}");
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+
+//string str = "a" == "b" ? "Ура" : "(";
+
+//Console.WriteLine($"Наибольшую цифру числа -> {maxDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшую цифру числа -> {maxDigit}");
