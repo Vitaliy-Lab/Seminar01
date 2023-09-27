a639d35e52496b00e7e374ff9905525e9e1052dc
Vitaliy-lab <9175603000@mail.ru>
2023-09-27 18:13:45 +0300

    Внёс изменения в задачу в папке Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -14,4 +14,5 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int square = number * number;
 
-Console.WriteLine(number + "->" + square);
+Console.WriteLine(number + " -> " + square);
+Console.WriteLine($"{number} -> {square}");
